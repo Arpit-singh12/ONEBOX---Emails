e6a0713edb5e1d4fcd551bc651e9ba0244d5a08d
Arpit-singh12 <singharpit20034@gmail.com>
2025-09-25 22:18:59 +0530

    restructuring root folder

--- frontend/postcss.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
-// This file is used to configure PostCSS, which is a tool for transforming CSS with JavaScript.
